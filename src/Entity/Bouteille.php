a6c8ec3b67b9b312bc39fd104de5ece010df1169
Marion <marionbailleux07@gmail.com>
2023-01-17 14:54:42 +0100

    Getteur et setteur

--- src/Entity/Bouteille.php
+++ src/Entity/Bouteille.php
@@ -26,4 +26,93 @@ class Bouteille
 
     #[ORM\Column]
     private ?int $quantity = null;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     */
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    /**
+     * @param string|null $marque
+     */
+    public function setMarque(?string $marque): void
+    {
+        $this->marque = $marque;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAlcoholType(): ?string
+    {
+        return $this->alcohol_type;
+    }
+
+    /**
+     * @param string|null $alcohol_type
+     */
+    public function setAlcoholType(?string $alcohol_type): void
+    {
+        $this->alcohol_type = $alcohol_type;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param int|null $price
+     */
+    public function setPrice(?int $price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param int|null $quantity
+     */
+    public function setQuantity(?int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+
 }
