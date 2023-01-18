d95046942453fa642b54e9d9ab1ced0e448addf9
MarieJm <jomain.marie@gmail.com>
2023-01-18 11:01:57 +0100

    alerte stock

--- src/Controller/BouteilleController.php
+++ src/Controller/BouteilleController.php
@@ -11,19 +11,27 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BouteilleController extends AbstractController {
     
-    #[Route('/liste_bouteilles', name: 'liste_bouteilles')]
-    public function listeBouteille(ManagerRegistry $doctrine): Response {
+    #[Route('/liste_bouteilles_all', name: 'liste_bouteilles')]
+    public function listeBouteilleAll(ManagerRegistry $doctrine): Response {
         $bouteilles = $doctrine->getRepository(Bouteille::class)->findAll();
         $resultat = "";
+        $alerte = "";
 
         foreach($bouteilles as $bouteille) {
-            $resultat .= $bouteille->getName() . " " .$bouteille->getQuantity() . "<br>";
+            if ($bouteille->getQuantity() < 10) {
+               $alerte = "<i style='color:red'> Stock très faible</i>";
+            } elseif ($bouteille->getQuantity() >= 10 && $bouteille->getQuantity() < 50) {
+                $alerte = "<i style='color:orange'> Stock faible</i>";
+            } else {
+                $alerte = "<i style='color:green'> Stock bon</i>";
+            }
+            $resultat .= $bouteille->getName() . " " .$bouteille->getQuantity() . " " .$alerte . "<br>";
         }
 
         return new Response("Quantités en stock : <br>" . $resultat);
     }
 
-    #[Route('/liste_bouteilles/{price}', name: 'liste_bouteilles')]
+    #[Route('/liste_bouteilles/{price}', name: 'liste_bouteilles_price')]
     public function findByPrice(ManagerRegistry $doctrine, BouteilleRepository $bouteilleRepo, int $price): Response {
         $bouteilles = $bouteilleRepo->findByPricePlus($price);
         $resultat = "";
@@ -35,7 +43,7 @@ class BouteilleController extends AbstractController {
         return new Response("Quantités en stock : <br>" . $resultat);
     }
 
-    #[Route('/liste_bouteilles/orderByCritere/{critere}', name: 'liste_bouteilles')]
+    #[Route('/liste_bouteilles/orderByCritere/{critere}', name: 'liste_bouteilles_critere')]
     public function orderByCritere(ManagerRegistry $doctrine, BouteilleRepository $bouteilleRepo, string $critere): Response {
         $bouteilles = $bouteilleRepo->orderByCritere($critere);
         $resultat = "";
@@ -48,7 +56,7 @@ class BouteilleController extends AbstractController {
     }
 
     
-    #[Route('/liste_bouteilles/orderByCritereWhereSup/{critere}/{numb}', name: 'liste_bouteilles')]
+    #[Route('/liste_bouteilles/orderByCritereWhereSup/{critere}/{numb}', name: 'liste_bouteilles_numb')]
     public function orderByCritereWhereSup(ManagerRegistry $doctrine, BouteilleRepository $bouteilleRepo, string $critere, string $numb): Response {
         $bouteilles = $bouteilleRepo->orderByCritereWhereSup($critere, $numb);
         $resultat = "";
@@ -59,4 +67,5 @@ class BouteilleController extends AbstractController {
 
         return new Response("Liste de bouteilles rangé par " . $critere . " :<br>" . $resultat);
     }
+
 }
